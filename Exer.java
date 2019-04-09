d466c0409de42c0807e8e79e849027ed0290078a
ashu-nayal <30870006+ashu-nayal@users.noreply.github.com>
2019-04-09 23:03:20 +0530

    Add files via upload

---
+++ Exer.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.Math;
+class Exer
+{
+	public static void main(String S[])
+	{
+		String txt="Hello World";
+		int range=10;
+		int c=(int)(Math.random()*range);
+		//System.out.println;
+		
+		System.out.println(txt.indexOf("l"));
+		//System.out.println(txt);
+		int i=0;
+		for(i=0;i<txt.length();i++)
+		{	
+			
+			System.out.println(txt);
+			System.out.println(c);
+		}
+	}
+}
